f891b5e4c844058c7a5fc80b6107b80e81276902
puskarwagle <puskarwagle17@gmail.com>
2022-11-15 02:05:03 +0545

    new everything

---
+++ app.js
@@ -0,0 +1,28 @@
+// Load Node modules
+var express = require('express');
+const ejs = require('ejs');
+const port = 8080
+
+// Initialise Express
+var app = express();
+// Render static files
+app.use(express.static('public'));
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+// Port website will run on
+
+
+app.listen(port, () => {
+    console.log(`App is now listening on: http://localhost:${port}`)
+})
+
+// Route Route
+app.get('/', function (req, res) {
+    var name = "Louise";
+    // Render index page
+    res.render('pages/index', {
+        // EJS variable and server-side variable
+        name: name
+    });
+});
+
